d6933d6289c14fb73b13ea46c0db9d4e6fe7ef2e
mhayescs19 <mhayescs19@gmail.com>
2020-03-10 09:49:13 -0700

    initial commit

---
+++ SwiftPortfolio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPortfolio
+//
+//  Created by Hayes, Michael on 3/9/20.
+//  Copyright © 2020 Hayes, Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
