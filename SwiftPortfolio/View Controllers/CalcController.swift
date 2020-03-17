397cd1176de3ae1a573bd4b982717af4ee9a5f8f
mhayescs19 <mhayescs19@gmail.com>
2020-03-16 22:23:37 -0700

    added calculator storyboard and "pipes" of calc label logic

---
+++ SwiftPortfolio/View Controllers/CalcController.swift
@@ -0,0 +1,71 @@
+//
+//  CalcController.swift
+//  SwiftPortfolio
+//
+//  Created by Michael Hayes on 3/16/20.
+//  Copyright © 2020 Hayes, Michael. All rights reserved.
+//
+
+import UIKit
+
+class CalcController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func concatCalcAreaLabel(keyNumber: String){
+        <#function body#>
+    }
+    @IBAction func press1(_ sender: UIButton) {
+        let keyNumber:(String) = "1"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    @IBAction func press2(_ sender: UIButton) {
+        let keyNumber:(String) = "2"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    @IBAction func press3(_ sender: UIButton) {
+        let keyNumber:(String) = "3"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    @IBAction func press4(_ sender: UIButton) {
+        let keyNumber:(String) = "4"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    @IBAction func press5(_ sender: UIButton) {
+        let keyNumber:(String) = "5"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    @IBAction func press6(_ sender: UIButton) {
+        let keyNumber:(String) = "6"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    @IBAction func press7(_ sender: UIButton) {
+        let keyNumber:(String) = "7"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    @IBAction func press8(_ sender: UIButton) {
+        let keyNumber:(String) = "8"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    @IBAction func press9(_ sender: UIButton) {
+        let keyNumber:(String) = "9"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    @IBAction func press0(_ sender: UIButton) {
+        var keyNumber:(String) = "0"
+        concatCalcAreaLabel(keyNumber: keyNumber)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
