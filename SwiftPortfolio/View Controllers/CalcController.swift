64be98c31d37efded74c182894117b80612d93ab
mhayescs19 <mhayescs19@gmail.com>
2020-03-22 15:47:05 -0700

    added single operator functionality

--- SwiftPortfolio/View Controllers/CalcController.swift
+++ SwiftPortfolio/View Controllers/CalcController.swift
@@ -33,6 +33,54 @@ class CalcController: UIViewController {
         calcAreaLabel.text = " " // makes the calculator display a blank label
         // Do any additional setup after loading the view.
     }
+    func calculateIt(value1: Double, value2: Double, mathOp: Int) -> Float {
+        switch mathOp {
+                    case PLUS:
+                        //   arg2 = calcAreaNumber.doubleValue;
+                        calcAnswer = Float(value1 + value2)
+                        break
+                    case MINUS:
+                        //  arg2 = calcAreaNumber.doubleValue;
+                        calcAnswer = Float(value1 - value2)
+                        break
+                    case DIVIDE:
+                      //   arg2 = calcAreaNumber.doubleValue;
+                        calcAnswer = Float(value1 / value2)
+                        break
+                    case MULTIPLY:
+                       // arg2 = calcAreaNumber.doubleValue;
+                        calcAnswer = Float(value1 * value2)
+                        break
+                    case MODULO:
+                        //arg2 = calcAreaNumber.doubleValue;
+                        calcAnswer = Float(Int(value1) % Int(value2))
+                        break
+                    case SQR:
+                        calcAnswer = Float(pow(value1, 2))
+                        //calcAnswer = arg1 * arg1;
+                        break
+                    case SQRT:
+                        calcAnswer = Float(pow(value1, 0.5))
+                        //calcAnswer = arg1 * arg1;
+                        break
+                    case CUBE:
+                        calcAnswer = Float((value1 * value1) * value1)
+                        break
+                    case FRACTION:
+                        calcAnswer = Float(1 / value1)
+                        break
+                    case -1:
+                        calcAnswer = Float(value1)
+                        break
+                    default:
+                        break
+                }
+            return calcAnswer
+        }
+    func calculateAnswer(){
+        value2 = Double(calcAreaLabel.text!)!
+        calcAnswer = calculateIt(value1: value1, value2: value2, mathOp: mathOp)
+    }
     func saveOperator(opNumber: Int){
         mathOp = opNumber
     }
@@ -56,6 +104,18 @@ class CalcController: UIViewController {
         }
         setCalcAreaLabel() // goes to set label helper
     }
+    func saveValueofAnswer() {  // method to save value of answer after calc to arg1
+        mathOp = -1;          // operator is unassigned after calc
+        value1 = 0.0     // arg1 is current display value
+        value2 = 0.0            // arg2 is now unassigned
+        calcAreaNumber = String(calcAnswer)
+    }
+    @IBAction func pressEqual(_ sender: UIButton) {
+        calculateAnswer()
+        saveValueofAnswer()
+        setCalcAreaLabel()
+    }
+    
     @IBAction func press1(_ sender: UIButton) {
         let keyNumber:(String) = "1"
         concatCalcAreaLabel(keyNumber: keyNumber) // (what the new variable is going to be in the func: variable from current function)
